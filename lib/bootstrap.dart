55bd96cd35f945539d1dc248e938cc175f83ac92
LuckUVeryX <luckuveryx@icloud.com>
2024-06-17 18:54:20 +0800

    bootstrap: init supabase

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,13 +2,19 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/widgets.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
-  // Add cross-flavor configuration here
+  await Supabase.initialize(
+    url: const String.fromEnvironment('SUPABASE_URL'),
+    anonKey: const String.fromEnvironment('SUPABASE_ANON_KEY'),
+  );
 
   runApp(await builder());
 }

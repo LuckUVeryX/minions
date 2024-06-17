bf3c7f705c741ea26cb1bba6efa3ea194377ef4c
LuckUVeryX <luckuveryx@icloud.com>
2024-06-17 19:38:27 +0800

    pubspec: add shadcn UI

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:minions/l10n/l10n.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        useMaterial3: true,
-      ),
+    return const ShadApp(
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
     );
--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:minions/app/app.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders MaterialApp', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(MaterialApp), findsOneWidget);
+      await tester.pumpAndSettle();
+      expect(find.byType(ShadApp), findsOneWidget);
     });
   });
 }

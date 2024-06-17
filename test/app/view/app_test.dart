9436f5568b6a56fe52728658b566d31446506467
LuckUVeryX <luckuveryx@icloud.com>
2024-06-18 00:33:44 +0800

    fix: override authStatusProvider in test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,13 +1,30 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:minions/app/app.dart';
+import 'package:minions/features/auth/auth.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../helpers/helpers.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders ShadApp', (tester) async {
-      await tester.pumpWidget(const ProviderScope(child: RouterApp()));
+      final fakeAuthState = AuthState(
+        AuthChangeEvent.initialSession,
+        fakeSession,
+      );
+
+      await tester.pumpWidget(
+        ProviderScope(
+          overrides: [
+            authStatusProvider.overrideWith((_) => Stream.value(fakeAuthState)),
+          ],
+          child: const RouterApp(),
+        ),
+      );
       await tester.pumpAndSettle();
+
       expect(find.byType(ShadApp), findsOneWidget);
     });
   });

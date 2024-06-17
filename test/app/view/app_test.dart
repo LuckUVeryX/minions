424aa6dbfecf03262d7e48980be2ef895b15dae7
LuckUVeryX <luckuveryx@icloud.com>
2024-06-18 00:05:35 +0800

    fix: add providerscope to tests

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,11 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:minions/app/app.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders MaterialApp', (tester) async {
-      await tester.pumpWidget(const RouterApp());
+    testWidgets('renders ShadApp', (tester) async {
+      await tester.pumpWidget(const ProviderScope(child: RouterApp()));
       await tester.pumpAndSettle();
       expect(find.byType(ShadApp), findsOneWidget);
     });

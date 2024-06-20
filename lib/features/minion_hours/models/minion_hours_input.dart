bb1b932a62cb59f6b3e351cb58aaddc283039799
Ryan Yip <71072280+LuckUVeryX@users.noreply.github.com>
2024-06-20 12:01:03 +0800

    Feat/input minion hours (#7)
    * feat: minon hours input
    
    * analysis: ignore lints
    
    * make: web-server and watch shorthands

--- lib/features/minion_hours/models/minion_hours_input.dart
+++ lib/features/minion_hours/models/minion_hours_input.dart
@@ -1,19 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:minions/app/utils/time_of_day_extensions.dart';
 import 'package:minions/features/facilities/facilities.dart';
 
 part 'minion_hours_input.freezed.dart';
-part 'minion_hours_input.g.dart';
 
 @freezed
 class MinionHoursInput with _$MinionHoursInput {
   const factory MinionHoursInput({
     Facility? facility,
-    DateTime? start,
-    DateTime? end,
+    DateTime? date,
+    TimeOfDay? startTime,
+    TimeOfDay? endTime,
+    @Default(false) bool lunchBreak,
   }) = _MinionHoursInput;
 
   const MinionHoursInput._();
 
-  factory MinionHoursInput.fromJson(Map<String, dynamic> json) =>
-      _$MinionHoursInputFromJson(json);
+  bool isValid() {
+    final start = startTime;
+    final end = endTime;
+
+    return facility != null &&
+        date != null &&
+        start != null &&
+        end != null &&
+        start.isBefore(end);
+  }
 }
--- lib/features/minion_hours/providers/minion_hours_controller.dart
+++ lib/features/minion_hours/providers/minion_hours_controller.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:minions/app/utils/time_of_day_extensions.dart';
 import 'package:minions/features/facilities/facilities.dart';
 import 'package:minions/features/minion_hours/minion_hours.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -15,11 +17,30 @@ class MinionHoursController extends _$MinionHoursController {
     state = state.copyWith(facility: value);
   }
 
-  void onStartChanged(DateTime value) {
-    state = state.copyWith(start: value);
+  void onDateChanged(DateTime value) {
+    state = state.copyWith(date: value);
   }
 
-  void onEndChanged(DateTime value) {
-    state = state.copyWith(end: value);
+  void onStartTimeChanged(TimeOfDay value) {
+    final start = value.quarter();
+    state = state.copyWith(startTime: start);
+
+    final end = state.endTime;
+    if (end != null && end.isBefore(start)) {
+      state = state.copyWith(endTime: start);
+    }
+  }
+
+  void onEndTimeChanged(TimeOfDay value) {
+    final end = value.quarter();
+    state = state.copyWith(endTime: end);
+    final start = state.startTime;
+    if (start != null && start.isAfter(end)) {
+      state = state.copyWith(startTime: end);
+    }
+  }
+
+  void onLunchBreakChanged(bool value) {
+    state = state.copyWith(lunchBreak: value);
   }
 }

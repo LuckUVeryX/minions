f625240e9bbe999b5546a1057a40914d03539723
LuckUVeryX <luckuveryx@icloud.com>
2024-06-22 22:46:32 +0800

    feat: better listenError messages

--- lib/app/utils/ref_extensions.dart
+++ lib/app/utils/ref_extensions.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:minions/app/app.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 extension WidgetRefX on WidgetRef {
   void listenError<T extends AsyncValue<dynamic>>(
@@ -10,9 +11,16 @@ extension WidgetRefX on WidgetRef {
     return listen(
       provider,
       (_, v) => v.whenOrNull(
-        error: (e, _) => context.toaster.show(
-          ShadToast.destructive(title: Text('$e')),
-        ),
+        error: (e, _) {
+          final message = switch (e) {
+            AuthException(:final message) => message,
+            PostgrestException(:final message) => message,
+            _ => '$e',
+          };
+          context.toaster.show(
+            ShadToast.destructive(title: Text(message)),
+          );
+        },
       ),
     );
   }

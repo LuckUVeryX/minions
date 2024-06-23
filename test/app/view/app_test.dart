213991d0ffb1584de5b467af8109290889e98d62
LuckUVeryX <luckuveryx@icloud.com>
2024-06-23 17:07:46 +0800

    fix: test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,30 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:minions/app/app.dart';
-import 'package:minions/features/auth/auth.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../helpers/helpers.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders ShadApp', (tester) async {
-      final fakeAuthState = AuthState(
-        AuthChangeEvent.initialSession,
-        fakeSession,
-      );
-
-      await tester.pumpWidget(
-        ProviderScope(
-          overrides: [
-            authStatusProvider.overrideWith((_) => Stream.value(fakeAuthState)),
-          ],
-          child: const RouterApp(),
-        ),
-      );
+      await tester.pumpApp(const Offstage());
       await tester.pumpAndSettle();
-
       expect(find.byType(ShadApp), findsOneWidget);
     });
   });
